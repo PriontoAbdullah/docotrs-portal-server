24f4d8f7efc1f37d2982e07554041f829812027a
PriontoAbdullah <priontosoft@gmail.com>
2021-01-17 20:24:10 +0600

    Patients get post api request done

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 	.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect((err) => {
 	const doctorCollection = client.db('doctorsPortal').collection('doctors');
+	const appointmentCollection = client.db('doctorsPortal').collection('appointments');
 
 	console.log('Doctors Portal DataBase Connected');
 
@@ -22,7 +24,6 @@ client.connect((err) => {
 	// Root Route
 	app.get('/', (req, res) => res.send('Welcome to Doctors Portal Backed'));
 
-    
 	// Get all services Information
 	app.get('/doctors', (req, res) => {
 		doctorCollection.find({}).toArray((err, documents) => {
@@ -30,19 +31,33 @@ client.connect((err) => {
 		});
 	});
 
+	// Get all Booked Appointments
+	app.get('/bookedAppointments', (req, res) => {
+		appointmentCollection.find({}).toArray((err, documents) => {
+			res.send(documents);
+		});
+	});
 
 	//Routes -- Post method
 	// Added all doctors Information
 	app.post('/addDoctor', (req, res) => {
-		const data = req.body;
-		doctorCollection.insertMany(data).then((result) => {
-			console.log(result);
+		const doctorData = req.body;
+		doctorCollection.insertMany(doctorData).then((result) => {
 			console.log(result.insertedCount, 'All Data Inserted');
 			res.send(result.insertedCount);
 		});
-    });
+	});
 
+	// Insert Appointment Booking
+	app.post('/makeBooking', (req, res) => {
+		const appointmentData = req.body;
+		appointmentCollection.insertOne(appointmentData, (err, result) => {
+			console.log(result.insertedCount, 'Appointment Inserted');
+			res.send(result.insertedCount > 0);
+		});
+    });
     
+
 });
 
 const port = process.env.PORT || 5000;
