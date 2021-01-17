4b32fdb2a943c89ce4c65aab812b314f156ed424
PriontoAbdullah <priontosoft@gmail.com>
2021-01-17 16:29:46 +0600

    Docotrs method done

--- index.js
+++ index.js
@@ -9,16 +9,41 @@ const app = express();
 app.use(cors());
 app.use(BodyParser.json());
 
-const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9ypd.mongodb.net/${process.env
-.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9ypd.mongodb.net/${process.env
+	.DB_NAME}?retryWrites=true&w=majority`;
 
-let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+	const doctorCollection = client.db('doctorsPortal').collection('doctors');
 
-//Routes -- Get method
-// Root Route
-app.get('/', (req, res) => res.send('Welcome to Doctors Portal Backed'));
+	console.log('Doctors Portal DataBase Connected');
+
+	//Routes -- Get method
+	// Root Route
+	app.get('/', (req, res) => res.send('Welcome to Doctors Portal Backed'));
+
+    
+	// Get all services Information
+	app.get('/doctors', (req, res) => {
+		doctorCollection.find({}).toArray((err, documents) => {
+			res.send(documents);
+		});
+	});
 
 
+	//Routes -- Post method
+	// Added all doctors Information
+	app.post('/addDoctor', (req, res) => {
+		const data = req.body;
+		doctorCollection.insertMany(data).then((result) => {
+			console.log(result);
+			console.log(result.insertedCount, 'All Data Inserted');
+			res.send(result.insertedCount);
+		});
+    });
+
+    
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, (err) => (err ? console.log('Filed to Listen on Port', port) : console.log('Listing for Port', port)));
